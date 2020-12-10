079679677ed3a88f9749082511ffaea74b2c4ad8
Lucas Castro <lucas.sc98@hotmail.com>
2020-12-10 10:26:04 -0300

    Not found page

--- src/components/layouts/Content.jsx
+++ src/components/layouts/Content.jsx
@@ -5,6 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import Home from "../../views/examples/Home";
 import Param from "../../views/examples/Param";
 import About from "../../views/examples/About";
+import NotFound from "../../views/examples/NotFound";
 
 const Content = props => (
   <main className="Content">
@@ -17,6 +18,9 @@ const Content = props => (
       </Route>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route exact path="*">
+        <NotFound />
       </Route>
     </Switch>
   </main>
---
+++ src/views/examples/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NotFound = (props) => (
+  <div className="NotFound">
+    <h1>Página não encontrada!</h1>
+    <h2>Opa! Acho que o conteúdo que você tenta encontrar não está disponível.</h2>
+  </div>
+);
+
+export default NotFound;
